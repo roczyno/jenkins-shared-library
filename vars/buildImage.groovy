c4c058cf5bf1f4d6fa491b137dc95c49abec33e3
roczyno <adiabaroczyno9@gmail.com>
2025-04-15 14:46:11 +0000

    sgs

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,11 @@
 #!/usr/bin/env groovy
 
-def call(){
+def call(String imageName){
     echo 'building the docker image...'
     withCredentials([usernamePassword(credentialsId:'docker-hub-rep-credentials',passwordVariable: 'PASS',usernameVariable: 'USER')]){
-        sh 'docker build -t roczyno/my-repo:jma-2.0 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER  --password-stdin"
-        sh 'docker push roczyno/my-repo:jma-2.0'
+        sh "docker push $imageName"
     }
 }
+
